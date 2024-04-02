667d8554bb543163009e6d483714892cb15a5589
Ingo Reitz <9l@9lo.re>
2024-04-02 08:26:52 +0200

    improve mediaplayer for waybar

--- linux-wm/waybar/mediaplayer.sh
+++ linux-wm/waybar/mediaplayer.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 player_status=$(playerctl status 2> /dev/null)
+text() {
+    if [[ -n $(playerctl metadata artist) ]]; then
+        echo "$(playerctl metadata artist) - $(playerctl metadata title)"
+    else
+        playerctl metadata title
+    fi
+    
+}
 if [ "$player_status" = "Playing" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo " $(text)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo " $(text)"
 fi
